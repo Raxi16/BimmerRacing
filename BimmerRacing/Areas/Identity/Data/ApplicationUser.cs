c966d03cacb9af3e29db6ecd7d24fbec0328d14a
Md Tusriful Rafi <ac119820@avcol.school.nz>
2024-03-13 10:10:49 +1300

    Added application user and made register and login work

---
+++ BimmerRacing/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿namespace BimmerRacing.Areas.Identity.Data
+{
+    public class ApplicationUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
