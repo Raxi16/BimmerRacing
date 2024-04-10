e6279b9d0113a75dc8ca5c2a475ed56b962d9949
Md Tusriful Rafi <ac119820@avcol.school.nz>
2024-04-10 13:24:34 +1200

    added cart

--- BimmerRacing/Areas/Identity/Data/ApplicationUser.cs
+++ BimmerRacing/Areas/Identity/Data/ApplicationUser.cs
@@ -2,7 +2,7 @@
 {
     public class ApplicationUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
     }
 }
--- BimmerRacing/Controllers/HomeController.cs
+++ BimmerRacing/Controllers/HomeController.cs
@@ -43,6 +43,21 @@ namespace BimmerRacing.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
