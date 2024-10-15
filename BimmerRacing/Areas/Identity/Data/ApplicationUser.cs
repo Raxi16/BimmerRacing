cd30dd845367feb68b45cfc075f325a2f0667ffa
Md Tusriful Rafi <ac119820@avcol.school.nz>
2024-10-15 14:06:06 +1300

    added validations to application user

--- BimmerRacing/Areas/Identity/Data/ApplicationUser.cs
+++ BimmerRacing/Areas/Identity/Data/ApplicationUser.cs
@@ -1,8 +1,15 @@
-﻿namespace BimmerRacing.Areas.Identity.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BimmerRacing.Areas.Identity.Data
 {
     public class ApplicationUser
     {
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public required string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
         public required string LastName { get; set; }
     }
 }
