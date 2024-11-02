c0d59212898cc169a2b1c8c12d91f0d313296bec
Md Tusriful Rafi <ac119820@avcol.school.nz>
2024-11-02 22:32:04 +1300

    updated contact.cshtml

--- BimmerRacing/Areas/Identity/Data/ApplicationUser.cs
+++ BimmerRacing/Areas/Identity/Data/ApplicationUser.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using BimmerRacing.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace BimmerRacing.Areas.Identity.Data
 {
